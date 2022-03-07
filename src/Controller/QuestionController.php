d1c540843610b6d7f23ba6081bfb482c9656c923
Sakshi Goraniya <sakshigoraniya13@gmail.com>
2022-03-07 13:39:34 +0530

    we are rocking in this symphony thing

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+      
+        return new Response("hello");
+    }
+     /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+      
+        return new Response(sprintf(
+            'Future page to show the question "%s"!',
+           ucwords(str_replace('-', ' ', $slug))
+        ));
+    }
+
+}
+
+
+
+?>
