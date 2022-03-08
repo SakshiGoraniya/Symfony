825f61e87b9eb5ce2b7a00af3c2d1cafb0350dd1
Sakshi Goraniya <sakshigoraniya13@gmail.com>
2022-03-08 15:52:02 +0530

    charming development in symfony 5

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,28 +1,39 @@
 <?php
 namespace App\Controller;
-
+use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class QuestionController
+
+class QuestionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/",name="app_homepage")
      */
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
-      
-        return new Response("hello");
+        // $html = $twigEnvironment->render('question/homepage.html.twig');
+        return $this->render('question/homepage.html.twig');
+        // return new Response($html);
     }
      /**
-     * @Route("/questions/{slug}")
+     * @Route("/questions/{slug}",name="app_question_show")
      */
     public function show($slug)
     {
+
+        $answers=['make sure your cat is sitting perfectly',
+                'furry shoes better than cat',
+                'try it backwards'    
+    ];
+    dump($slug,$this);
       
-        return new Response(sprintf(
-            'Future page to show the question "%s"!',
-           ucwords(str_replace('-', ' ', $slug))
-        ));
+        return $this->render('question/show.html.twig',[
+            'question'=> ucwords(str_replace('-', ' ', $slug)),
+            'answers'=>$answers,
+        ]);
+
+        
     }
 
 }
