41ba4f658ce2dc7d99a4b2f2b4d14564f8dd9e15
Sakshi Goraniya <sakshigoraniya13@gmail.com>
2022-03-10 15:50:11 +0530

    Services, Config & Environments

--- src/service/MarkdownHelper.php
+++ src/service/MarkdownHelper.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\service;
+use  Psr\Log\LoggerInterface;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
@@ -8,17 +9,30 @@ class MarkdownHelper
 {
     private $markdownParser;
     private $cache;
+    private $isDebug;
+    private $logger;
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug,LoggerInterface $mdLogger)
     {
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        $this->logger=$mdLogger;
+        //dump($isDebug);
     }
 
     public function parse(string $source): string
     {
+        
+        if (stripos($source, 'cat') !== false) {
+            $this->logger->info('Meow!');
+        }
+        if($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
         return $this->cache->get('markdown_'.md5($source), function() use ($source) {
             return $this->markdownParser->transformMarkdown($source);
+            
         });
     }
 }
