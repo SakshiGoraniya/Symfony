d4a00561433eaffbb07d55d4571b857972a0ed69
Sakshi Goraniya <sakshigoraniya13@gmail.com>
2022-03-29 15:57:18 +0530

    2fa authentication

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App\Controller;
-
+use Endroid\QrCode\Builder\Builder;
+use Doctrine\ORM\EntityManagerInterface;
+use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Totp\TotpAuthenticatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
-class SecurityController extends AbstractController
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Scheb\TwoFactorBundle\Security\TwoFactor\QrCode\QrCodeGenerator;
+class SecurityController extends BaseController
 {
     /**
      * @Route("/login", name="app_login")
@@ -27,5 +30,28 @@ class SecurityController extends AbstractController
     {
         throw new \Exception("logout() should never be reached.");
         
+    }
+     /**
+     * @Route("/authentication/2fa/enable", name="app_2fa_enable")
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     */
+    public function enable2fa(TotpAuthenticatorInterface $totpAuthenticator, EntityManagerInterface $entityManager)
+    {
+        $user = $this->getUser();
+        if (!$user->isTotpAuthenticationEnabled()) {
+            $user->setTotpSecret($totpAuthenticator->generateSecret());
+            $entityManager->flush();
+        }
+       return $this->render('security/enable2fa.html.twig');
+    }
+     /**
+     * @Route("/authentication/2fa/qr-code", name="app_qr_code")
+     */
+    public function displayGoogleAuthenticatorQrCode(QrCodeGenerator $qrCodeGenerator)
+    {
+        // $qrCode is provided by the endroid/qr-code library. See the docs how to customize the look of the QR code:
+        // https://github.com/endroid/qr-code
+        $qrCode = $qrCodeGenerator->getTotpQrCode($this->getUser());
+        return new Response($qrCode->writeString(), 200, ['Content-Type' => 'image/png']);
     }
 }
